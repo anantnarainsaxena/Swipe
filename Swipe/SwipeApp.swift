78eaaf273fc0a6196c2c3b1ad21a71947a28de41
anantnarainsaxena <anantnarain2772001@gmail.com>
2024-01-15 01:57:12 +0530

    Initial Commit

---
+++ Swipe/SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeApp.swift
+//  Swipe
+//
+//  Created by Anant Narain on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
